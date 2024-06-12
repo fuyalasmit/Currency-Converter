7379d8265d64dae0c01cd52bc1234f00a7eb72be
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-06-12 22:30:41 +0545

    Completed but API not working!

---
+++ script.js
@@ -0,0 +1,71 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg")
+
+//namellist 
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        //usd npr const 
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        }
+        else if (select.name === "to" && currCode === "NPR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    //flag change-1
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    })
+}
+
+//flag change-2
+const updateFlag = (element) => {
+    let currCode = element.value;
+    console.log(currCode);
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    console.log(amtVal);
+    if (amtVal === "" || amtVal <= 0) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    console.log(fromCurr.value, toCurr.value);
+    // const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+
+}
+
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
